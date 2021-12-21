efba3bd060746d8d5bfb81063c9f622eefac47f8
olena karaim <76853056+karakumm@users.noreply.github.com>
2021-12-21 14:40:18 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
